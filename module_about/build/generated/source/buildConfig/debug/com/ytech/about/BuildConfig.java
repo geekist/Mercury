efa7b972c359885c3088e4381e283a9947b241b0
geekist <geekist@outlook.com>
2020-12-23 21:07:58 +0800

    add modules to project

---
+++ module_about/build/generated/source/buildConfig/debug/com/ytech/about/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ytech.about;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.ytech.about";
+  public static final String BUILD_TYPE = "debug";
+}
